8f10e7c1f49a9035bd865a91e6ee33682c86990c
Shubham Kumar <76239800+shubham1910200@users.noreply.github.com>
2022-08-16 21:27:53 +0530

    Add files via upload

---
+++ number triangular patter.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int n;
+cin>>n;
+int i=0;
+while(i<n)
+{
+	int j=0;
+	while(j<i)
+	{
+		cout<<i;
+		j++;
+	}
+	i++;
+	cout<<endl;
+	
+}
+}
